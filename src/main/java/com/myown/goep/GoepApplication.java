e1946e5827ac38aeae806a253eb5e9000650acae
Kamlesh Dev <kamlesh.dev@gmail.com>
2021-12-09 12:14:19 +0530

    goep springboot project created

---
+++ src/main/java/com/myown/goep/GoepApplication.java
@@ -0,0 +1,13 @@
+package com.myown.goep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoepApplication.class, args);
+	}
+
+}
